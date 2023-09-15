136ef680046b7bb7ad833b59020081615ebee2a4
soldiertt <soldiertt@gmail.com>
2023-09-15 19:48:35 +0200

    improvements

--- src/app/core/parameters.ts
+++ src/app/core/parameters.ts
@@ -1,3 +1,3 @@
 export const CANCEL_DEADLINE_HOURS: number = 24
-export const MAX_PARTICIPANTS_COURSE: number = 10
+export const MAX_PARTICIPANTS_COURSE: number = 8
 export const CARD_EXPIRATION_MONTHS: number = 6
