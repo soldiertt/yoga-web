ccd3d83ef09e447793e0b3b81de47f6ea9e7bd69
soldiertt <soldiertt@gmail.com>
2025-06-21 22:42:47 +0200

    update prices

--- src/app/public/components/my-cards/my-cards.component.ts
+++ src/app/public/components/my-cards/my-cards.component.ts
@@ -7,6 +7,7 @@ import {MatDialog} from '@angular/material/dialog';
 import {Select, Store} from '@ngxs/store';
 import {PublicState} from '../../state/public-state';
 import {Observable} from 'rxjs';
+import {LONG_CARD_PRICE, SHORT_CARD_PRICE} from '../../constants';
 
 @Component({
   selector: 'yog-my-cards',
@@ -31,7 +32,7 @@ export class MyCardsComponent {
     const dialogConfig = {
       data: {
         title: 'Commander une carte',
-        htmlContent: `<p>Vous êtes sur le point de commander une nouvelle carte pour 10 séances de Yoga. Prix: 150€, validité: 1 an.</p>
+        htmlContent: `<p>Vous êtes sur le point de commander une nouvelle carte pour 10 séances de Yoga. Prix: ${LONG_CARD_PRICE} €, validité: 1 an.</p>
                 <p>Confirmez ?</p>
                 `
       }
@@ -48,7 +49,7 @@ export class MyCardsComponent {
     const dialogConfig = {
       data: {
         title: 'Commander une carte',
-        htmlContent: `<p>Vous êtes sur le point de commander une nouvelle carte pour 10 séances de Yoga. Prix: 135€, validité: 30 juin 2025.</p>
+        htmlContent: `<p>Vous êtes sur le point de commander une nouvelle carte pour 10 séances de Yoga. Prix: ${SHORT_CARD_PRICE} €, validité: 30 juin 2025.</p>
                 <p>Confirmez ?</p>
                 `
       }
@@ -61,4 +62,6 @@ export class MyCardsComponent {
     });
   }
 
+  protected readonly SHORT_CARD_PRICE = SHORT_CARD_PRICE;
+  protected readonly LONG_CARD_PRICE = LONG_CARD_PRICE;
 }
---
+++ src/app/public/constants.ts
@@ -0,0 +1,2 @@
+export const SHORT_CARD_PRICE = 140;
+export const LONG_CARD_PRICE = 160;
