b01568cbc9ac143a974a967bbdb4a69877842f05
soldiertt <soldiertt@gmail.com>
2025-12-15 19:53:40 +0100

    change expiration date

--- src/app/public/constants.ts
+++ src/app/public/constants.ts
@@ -1,3 +1,3 @@
 export const SHORT_CARD_PRICE = 140;
 export const LONG_CARD_PRICE = 160;
-export const SHORT_CARD_EXPIRATION = '12 décembre 2025';
+export const SHORT_CARD_EXPIRATION = '10 avril 2026';
